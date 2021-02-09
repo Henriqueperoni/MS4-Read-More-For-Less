7ddac8f3f227f5f3cf0e302bfb6cbb16b88dda11
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-09 15:17:41 +0000

    Initial setup

---
+++ read_more_for_less/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for read_more_for_less project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'read_more_for_less.settings')
+
+application = get_asgi_application()
