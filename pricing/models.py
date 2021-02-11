a6a12358981ae61d11bf2c2460e5c62e02885be8
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-11 08:26:20 +0000

    Add pricing app and models

---
+++ pricing/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Pricing(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=150)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    frequency = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
