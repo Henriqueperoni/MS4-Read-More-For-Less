b634af7623db4d3fdd66449fce9a1762515252c7
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-11 21:13:08 +0000

    Add add to bag functionality

--- cart/context.py
+++ cart/context.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 
+
 def cart_contents(request):
 
     cart_items = []
     total = 0
 
-
-
-    context ={
+    context = {
+        
         'cart_items': cart_items,
         'total': total,
     }
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,19 @@ def view_cart(request):
     """ A view that renders the cart content page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a pricing plan to the shopping cart"""
+
+    quantity = 1
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect('membership')
