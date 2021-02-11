475318759c176e86d79f4f1e79629cf26f8cf859
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-11 11:06:41 +0000

    Add pricing views and templates

--- pricing/urls.py
+++ pricing/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.plans_pricing, name='pricing'),
 ]
--- pricing/views.py
+++ pricing/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Pricing
 
 # Create your views here.
+
+
+def plans_pricing(request):
+    """ A view to show all the plans available """
+    pricing = Pricing.objects.all().order_by('price')
+
+    context = {
+        'pricing': pricing,
+    }
+
+    return render(request, 'pricing/pricing.html', context)
