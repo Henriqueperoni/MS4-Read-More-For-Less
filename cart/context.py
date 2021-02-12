bcfb9919bc7b112e098915e2f112ccfb431ca90b
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-12 09:43:09 +0000

    Update context processor and functionality to display cart items in the cart template

--- cart/context.py
+++ cart/context.py
@@ -1,13 +1,23 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from pricing.models import Pricing
 
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        plan = get_object_or_404(Pricing, pk=item_id)
+        total += quantity * plan.price
+        cart_items.append({
+            'item_id': item_id,
+            'plan': plan,
+        })
 
     context = {
-        
         'cart_items': cart_items,
         'total': total,
     }
