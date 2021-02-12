6f2e92b40ba744c9758d405b486db9897f25ca5a
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-12 12:13:11 +0000

    Add functionality to allow user add only one plan to the cart

--- cart/context.py
+++ cart/context.py
@@ -7,15 +7,18 @@ def cart_contents(request):
 
     cart_items = []
     total = 0
+    plan_count = 0
     cart = request.session.get('cart', {})
 
     for item_id, quantity in cart.items():
-        plan = get_object_or_404(Pricing, pk=item_id)
-        total += quantity * plan.price
-        cart_items.append({
-            'item_id': item_id,
-            'plan': plan,
-        })
+        if plan_count < 1:
+            plan_count += 1
+            plan = get_object_or_404(Pricing, pk=item_id)
+            total += quantity * plan.price
+            cart_items.append({
+                'item_id': item_id,
+                'plan': plan,
+            })
 
     context = {
         'cart_items': cart_items,
