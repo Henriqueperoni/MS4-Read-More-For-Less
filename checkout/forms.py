905a42856366b1cb8f6c3fab2840e440fc283de1
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-22 08:38:06 +0000

    Fix bug typo

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,7 +20,7 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'postcode': ' Postal Code',
+            'post_code': ' Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
--- checkout/models.py
+++ checkout/models.py
@@ -12,10 +12,10 @@ class Order(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=30, null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    post_code = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_adrdress1 = models.CharField(max_length=80, null=False, blank=False)
-    street_adrdress2 = models.CharField(max_length=80, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=40, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(
