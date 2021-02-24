81d49dca0da95376d90a6835a014e799d9b404f6
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-24 17:04:46 +0000

    Fix typo on stripe_pid

--- checkout/models.py
+++ checkout/models.py
@@ -22,7 +22,7 @@ class Order(models.Model):
     total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
     original_cart = models.TextField(null=False, blank=False, default='')
-    stripe_id = models.CharField(
+    stripe_pid = models.CharField(
         max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
