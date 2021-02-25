0bb2837803e6f60e0154538645d34bb7dfe9cb4a
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-25 17:50:21 +0000

    Update default login redirect url

--- read_more_for_less/settings.py
+++ read_more_for_less/settings.py
@@ -110,7 +110,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/success'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'read_more_for_less.wsgi.application'
 
