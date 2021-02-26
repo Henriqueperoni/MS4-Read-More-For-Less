69c7af1864674ce5c4d5d1cc01b59d18fa96c7ad
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-26 14:07:09 +0000

    Change frequency field

--- pricing/models.py
+++ pricing/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+frequency_types = [
+    ('monthly', 'Monthly'),
+    ('biannual', 'Biannual'),
+    ('yearly', 'Yearly')
+]
+
 
 class Pricing(models.Model):
 
@@ -11,7 +17,7 @@ class Pricing(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=150)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    frequency = models.CharField(max_length=15)
+    frequency = models.CharField(max_length=20, choices=frequency_types)
 
     def __str__(self):
         return self.name
