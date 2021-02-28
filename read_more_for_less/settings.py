0e6a06dbadca957e6ff870d966b15034996b7ccf
Henriqueperoni <henriqueperoni@hotmail.com>
2021-02-28 14:26:51 +0000

    Deploy and test AWS after change AWS_SECRET_ACCESS_KEY_ID to AWS_SECRET_ACCESS_KEY

--- read_more_for_less/settings.py
+++ read_more_for_less/settings.py
@@ -178,7 +178,7 @@ if 'USER_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'read-more-for-less'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
