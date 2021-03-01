8c371287253a65e1ed0a62ab39e3e263e781c42c
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-01 09:09:16 +0000

    Complete navbar for small screens

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,17 @@
 $('.toast').toast('show');
 
+// Burguer Navigation bar
+// Burguer menu inspiration from: https://www.youtube.com/watch?v=gXkqy0b4M5g
 const navSlide = () => {
-    const burguer = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links')
-
+    const burguer = document.querySelector(".burguer");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    // Active Burguer
     burguer.addEventListener("click", () => {
-        nav.ClassList.toggle('nav-active');
+        nav.classList.toggle("nav-active");
     });
+
 }
 
 navSlide();
