6d66ef2c499374560240590b197de42f6d4ce45c
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-09 08:27:35 +0000

    Update book_club view to display the book reviews on the book club template

--- book_club/views.py
+++ book_club/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import BookReview
+
 
 def book_club(request):
     """ A view to return the book club page with book reviews """
+    reviews = BookReview.objects.all()
 
-    return render(request, 'book_club/book_club.html')
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'book_club/book_club.html', context)
