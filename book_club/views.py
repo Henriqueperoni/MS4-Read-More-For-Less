b18c001342d2819b95405f2e2ae6f7df1783fbae
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-09 15:26:44 +0000

    Create funcionality to create a new book review

--- book_club/views.py
+++ book_club/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 
 from .models import BookReview
 from .forms import CreateReviewForm
@@ -31,6 +32,19 @@ def view_review(request, review_id):
 
 def create_review(request):
     """  A view to return the create_review template and CreateReview form. """
+
+    if request.method == 'POST':
+        create_review_form = BookReview(
+            user=request.user,
+            book_name=request.POST.get('book_name'),
+            book_author=request.POST.get('book_author'),
+            book_review=request.POST.get('book_review'),
+            image_url=request.POST.get('image_url'),
+        )
+
+        create_review_form.save()
+        messages.success(request, "Review created successfully")
+        return redirect('book_club')
 
     context = {
         'form': CreateReviewForm
