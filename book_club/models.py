0b063e362bdfa5c22db632385108a753c80a8634
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-09 08:26:41 +0000

    Create BookReview model and registered it on the admin

--- book_club/models.py
+++ book_club/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class BookReview(models.Model):
+    """
+    A model to save and display book reviews
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_name = models.CharField(max_length=50, null=False, blank=False)
+    book_author = models.CharField(max_length=50, null=False, blank=False)
+    book_review = models.TextField(max_length=1500, null=False, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.book_name
