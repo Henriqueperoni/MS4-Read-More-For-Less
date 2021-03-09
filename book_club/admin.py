663b30a01b132a58d30a2b2edaa40e9513bbbbb6
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-09 16:02:07 +0000

    Create ReviewComment model and migrate it

--- book_club/admin.py
+++ book_club/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import BookReview
-# Register your models here.
+from .models import BookReview, ReviewComment
 
 
 admin.site.register(BookReview)
+admin.site.register(ReviewComment)
--- book_club/models.py
+++ book_club/models.py
@@ -6,9 +6,7 @@ from django.utils import timezone
 
 
 class BookReview(models.Model):
-    """
-    A model to save and display book reviews
-    """
+    """ A model to save and display book reviews. """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book_name = models.CharField(max_length=50, null=False, blank=False)
     book_author = models.CharField(max_length=50, null=False, blank=False)
@@ -18,3 +16,14 @@ class BookReview(models.Model):
 
     def __str__(self):
         return self.book_name
+
+
+class ReviewComment(models.Model):
+    """ A model to save and display comments on the book reviews. """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField(max_length=1500, null=False, blank=False)
+    review = models.ForeignKey(BookReview, on_delete=models.CASCADE)
+    date_commented = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user}' comment on {self.review}"
