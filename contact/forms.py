99ad505b44f875f5cd6a252f0c474b8e68f092a2
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-14 16:09:26 +0000

    Add functionality to send emails to contact form

--- contact/forms.py
+++ contact/forms.py
@@ -7,3 +7,25 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ('contact_reason', 'name', 'email',
                   'comments')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus no first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'contact_reason': '',
+            'name': 'Enter your Name',
+            'email': 'Enter you Email',
+            'comments': 'Let us know how we can best assist you',
+        }
+
+        self.fields['contact_reason'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]}'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
-from .forms import ContactForm
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.core.mail import send_mail
+import os
 
-# Create your views here.
+from .forms import ContactForm
 
 
 def contact(request):
     """ A view to return the contact page """
-    form = ContactForm()
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        customer_email = request.POST.get('email')
+        customer_query = request.POST.get('comments')
+        if form.is_valid():
+            form.save()
+
+            # Send Email
+            send_mail(
+                'Read More For Less - New Contact Form',
+                customer_query,
+                [customer_email],
+                os.environ.get('EMAIL'),
+            )
+            messages.success(request, 'Mail sent. We will be in touch soon.')
+            return redirect(reverse('home'))
+        else:
+            messages.error(
+                request,
+                'Contact request failed. Please, ensure the form is valid!')
+    else:
+        form = ContactForm()
 
     context = {
         'form': form
