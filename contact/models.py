d1c54213bcae3abc614a16b668009cf3c7ae6f95
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-14 10:17:58 +0000

    Create contact model and migrate it. And create contact form

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+contact_choices = [
+    ('general_query', 'General Query'),
+    ('technical_issue', 'Technical Issue'),
+    ('plan_query', 'Plan Query'),
+    ('delivery', 'Delivery'),
+]
+
+
+class Contact(models.Model):
+    """ Model for create Contact Form """
+    contact_reason = models.CharField(
+        max_length=50,
+        choices=contact_choices,
+        null=False,
+        blank=False,
+        default='general_query')
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
+    comments = models.TextField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.contact_reason} - {self.name}'
--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
 
 
 def contact(request):
     """ A view to return the contact page """
+    form = ContactForm()
 
-    return render(request, 'contact/contact.html')
+    context = {
+        'form': form
+    }
+
+    return render(request, 'contact/contact.html', context)
