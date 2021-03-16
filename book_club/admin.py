40197258df5714b3f315b4c0acfff54434a5e447
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-16 17:19:27 +0000

    Update the  ReviewComments to a Tabular inline in the admin.py

--- book_club/admin.py
+++ book_club/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import BookReview, ReviewComment
 
 
-admin.site.register(BookReview)
-admin.site.register(ReviewComment)
+class ReviewCommentAdminInline(admin.TabularInline):
+    model = ReviewComment
+    readonly_fields = ('user', 'comment_text', 'review', 'date_commented',)
+    ordering = ('-date_commented',)
+
+
+class BookReviewAdmin(admin.ModelAdmin):
+    inlines = (ReviewCommentAdminInline,)
+
+    fields = ('user', 'book_name', 'book_author',
+              'book_review', 'image_url', 'date_posted',)
+    ordering = ('-date_posted',)
+
+
+admin.site.register(BookReview, BookReviewAdmin)
