9a43908b1db839eb5156b2ff911b007d3e643793
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-17 14:26:44 +0000

    Delete send email functionality

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from django.core.mail import send_mail
-import os
 
 from .forms import ContactForm
 
@@ -11,18 +9,9 @@ def contact(request):
 
     if request.method == 'POST':
         form = ContactForm(request.POST)
-        customer_email = request.POST.get('email')
-        customer_query = request.POST.get('comments')
         if form.is_valid():
             form.save()
 
-            # Send Email
-            send_mail(
-                'Read More For Less - New Contact Form',
-                customer_query,
-                [customer_email],
-                os.environ.get('EMAIL'),
-            )
             messages.success(request, 'Mail sent. We will be in touch soon.')
             return redirect(reverse('home'))
         else:
