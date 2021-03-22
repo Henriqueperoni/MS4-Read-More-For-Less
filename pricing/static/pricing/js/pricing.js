3c76f6a3a60e86f4dee0af1a922105e65adcc715
Henriqueperoni <henriqueperoni@hotmail.com>
2021-03-22 08:12:20 +0000

    Fix minor bug with the footer position and move the static files from the pricing app to a separete folder

---
+++ pricing/static/pricing/js/pricing.js
@@ -0,0 +1,13 @@
+// Bootstrap Tabs
+        $('#myTab a').on('click', function (event) {
+            event.preventDefault()
+            $(this).tab('show')
+        });
+
+        // Plans Animation
+        if (window.matchMedia("(min-width: 1200px)").matches) {
+            const priceAnim = gsap.timeline({ defaults: { ease: "power1.out" } });
+            priceAnim.from("#pills-home > div:nth-child(2) > div" , { opacity: 0, duration: 1.8 }).delay(0.1);
+            priceAnim.from("#pills-home > div:nth-child(1) > div", { x: 200, opacity: 0, duration: 1.2 }, "-=1.2");
+            priceAnim.from("#pills-home > div:nth-child(3) > div", { x: -200, opacity: 0, duration: 1.2 }, "-=1.2");
+        };
